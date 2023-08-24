2f479e465ae9827f5bf2cbd635facf609b513b09
Anish-Malhotra <anishmalhotra97@gmail.com>
2023-08-24 18:57:00 -0400

    Add Graphiql GUI and graphene deps

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from graphene import Schema
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 
+
+schema = Schema()
 
 app = FastAPI()
+graphene_app = GraphQLApp(
+    on_get=make_graphiql_handler(),
+    schema=schema,
+)
 
-@app.get("/test")
-def get_test():
-    return "Hello test!"
+app.mount("/graphql", graphene_app)
