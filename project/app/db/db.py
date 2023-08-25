c78cca35b3beb9b0cceffe0761759472ecdb60b4
Anish-Malhotra <anishmalhotra97@gmail.com>
2023-08-24 20:20:09 -0400

    App in docker-compose + project structure refactor

--- project/app/db/db.py
+++ project/app/db/db.py
@@ -1,10 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.db.models import Base
 
 
-DB_URL = "postgresql+psycopg://root:secret@localhost:5432/spotify-insights-db"
+DB_URL = os.environ.get("DATABASE_CONN_STRING")
 
 engine = create_engine(DB_URL, echo=True)
 Session = sessionmaker(bind=engine)
